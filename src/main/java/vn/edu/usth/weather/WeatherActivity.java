6363c253340428b79dd1bd5ba75b55cc5ecccfaa
dinlhuongntt124 <dinlhuong@gmail.com>
2019-11-15 15:03:46 +0700

    Practical 3

--- src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -13,6 +13,11 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+        ForecastFragment firstFragment = new ForecastFragment();
+        // Add the fragment to the ' container' FrameLayout
+        getSupportFragmentManager(). beginTransaction(). add(
+                R.id.container, firstFragment). commit();
     }
 
     @Override
