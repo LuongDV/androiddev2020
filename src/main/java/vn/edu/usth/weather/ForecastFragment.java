e822aeb64597261549144b4177622cbf1f7b99fb
dinlhuongntt124 <dinlhuong@gmail.com>
2019-11-20 15:18:32 +0700

    Practical 4

--- src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -4,24 +4,32 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 
 
 public class ForecastFragment extends Fragment {
+    public ForecastFragment() {
+        // Required empty public constructor
+    }
 
+//    @Nullable
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        View v = new View(getContext());
+//        v.setBackgroundColor(0xFFFF0000);
+//        return v;
+//    }
+
+    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View v = new View(getContext());
-        v.setBackgroundColor(0xFFFF0000);
-        return v;
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_forecast, container, false);
     }
 }
